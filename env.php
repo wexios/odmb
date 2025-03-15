2647a49a31c106e1738a6224701406e7bb7cc35d
Sebastian Thomas <sebastianthomasmanithara@gmail.com>
2025-03-15 18:16:15 +0530

    Env remove

--- env.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'API_KEY' => '53656439',
-    'DB_HOST' => 'sql12.freesqldatabase.com',
-    'DB_USER' => 'sql12767476',
-    'DB_PASS' => 'sTjyndxkGV',
-    'DB_NAME' => 'sql12767476'
-];
